dd835bff83d91516f313e50be2909b2e2acd0cc5
dtgagnon <gagnon.derek@gmail.com>
2025-06-22 13:04:40 -0400

    test fix

--- nix/modules/system.nix
+++ nix/modules/system.nix
@@ -123,11 +123,11 @@ in
         AWS_S3_ENDPOINT_URL=${cfg.storage.protocol}://${cfg.storage.host}:${toString cfg.storage.port}
         AWS_S3_BUCKET_NAME=${cfg.storage.bucket}
       '') +
-      (mkIf cfg.email.enable ''
+      mkIf cfg.email.enable ''
         EMAIL_HOST=${cfg.email.host}
         EMAIL_PORT=${toString cfg.email.port}
         EMAIL_USE_TLS=${if cfg.email.useTLS then "1" else "0"}
-      '');
+      '';
 
     # Secret credentials environment file from secret files
     system.activationScripts.plane-credentials = ''
